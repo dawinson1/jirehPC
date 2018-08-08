ca6b4daf89da79f00512f23eda4163b5e129b97f
Santiago Garcia <santiagog0902@gmail.com>
2018-08-08 13:07:02 -0500

    iconos para formulario

--- application/view/empleado/formEmpleado.php
+++ application/view/empleado/formEmpleado.php
@@ -4,65 +4,99 @@
             <div class="box-header">
               <h3 class="box-title">Empleado</h3>
             </div>
-            <div class="box-body">  <!--<div class="input-group">
-                <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
-                <input type="email" class="form-control" placeholder="XD">
-
-              <div class="input-group">
-                <label for="">Nombres:</label>
-                <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
-                <input type="email" class="form-control" placeholder="Email">
-              </div>
-              <br>
-
-              <div class="input-group">
-                <label>Text</label>
-                <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
-                <input type="email" class="form-control" placeholder="Email">
-              </div><br>
-
-              <div class="input-group">
-              <label for="">Apellidos:</label>
-                <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
-                <input type="email" class="form-control" placeholder="Email">
-              </div><br>
-
-              <div class="input-group">
-              <label for="">Teléfono:</label>
-                <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
-                <input type="email" class="form-control" placeholder="Email">
-              </div><br>
-
-              <div class="input-group">
-              <label for="">Correo:</label>
-                <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
-                <input type="email" class="form-control" placeholder="Email">
-              </div> <br>
-
-              <div class="input-group">
-              <label for="">Id Rol:</label>
-                <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
-                <input type="email" class="form-control" placeholder="Email">
-              </div> <br>
-
-              <div class="input-group">
-              <label for="">Id Estado:</label>
-                <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
-                <input type="email" class="form-control" placeholder="Email">
-              </div><br>
-
-              <div class="input-group">
-              <label for="">  Cédula Empleado:</label>
-                <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
-                <input type="email" class="form-control" placeholder="Email">
-              </div><br>
+            <form>
+            <div class="box-body"> <!--Este Div es es contenedor de los imputs-->
               
-              
+                <div class="form-group"> <!--Comienzo del div contenedor del input-->
+                    <label for="identificador" >Documento de Identidad:</label>
 
-                </div>
-              </div>
-            </div>
-            
+                    <div class="input-group my-colorpicker2 colorpicker-element"> <!--comienzo div del inputt-->
+                        <span class="input-group-addon"><i class="fa fa-address-card"></i></span>
+                        <input type="text" class="form-control" placeholder="Ingrese documento del empleado"
+                            name="idEmpleado" id="idEmpleado">
+
+                        <div class="input-group-addon"> <!--Este div es opcional, servirá cuando queramos que en frente del input este otro icono-->
+                        <i class="glyphicon glyphicon-search"></i>
+                        </div> <!--Cierre del div opcional-->
+
+                    </div><!--cierre div del inputt-->
+                </div> <!--cierre del div contenedor del input-->
+
+                <div class="form-group"> <!--Comienzo del div contenedor del input-->
+                    <label for="nomCliente">Nombres:</label>
+
+                    <div class="input-group my-colorpicker2 colorpicker-element"> <!--comienzo div del inputt-->
+                        <span class="input-group-addon"><i class="fa fa-address-book-o"></i></span>
+                        <input type="text" class="form-control" placeholder="Ingrese nombre del empleado"
+                        name="nombreE" id="nombreE">
+
+                    </div><!--cierre div del inputt-->
+                </div> <!--cierre del div contenedor del input-->
+
+                <div class="form-group"> <!--Comienzo del div contenedor del input-->
+                    <label for="apeCliente">Apellidos:</label>
+
+                    <div class="input-group my-colorpicker2 colorpicker-element"> <!--comienzo div del inputt-->
+                        <span class="input-group-addon"><i class="fa fa-address-book-o"></i></span>
+                        <input type="text" class="form-control" placeholder="Ingrese apellidos del empleado"
+                        name="apellidoE" id="apellidoE">
+
+                    </div><!--cierre div del inputt-->
+                </div> <!--cierre del div contenedor del input-->
+
+                <div class="form-group"> <!--Comienzo del div contenedor del input-->
+                    <label for="correoCliente">Teléfono:</label>
+
+                    <div class="input-group my-colorpicker2 colorpicker-element"> <!--comienzo div del inputt-->
+                        <span class="input-group-addon"><i class="fa fa-phone-square"></i></span>
+                        <input type="email" class="form-control" placeholder="Teléfono"
+                         name="telefonoE" id="telefonoE">
+
+
+                    </div><!--cierre div del inputt-->
+                </div> <!--cierre del div contenedor del input-->
+
+                <div class="form-group"> <!--Comienzo del div contenedor del input-->
+                    <label for="direcCliente">Correo:</label>
+
+                    <div class="input-group my-colorpicker2 colorpicker-element"> <!--comienzo div del inputt-->
+                        <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
+                        <input type="text" class="form-control" placeholder="Ingrese la direccion de su hogar"
+                        name="correoE" id="correoE">
+
+                    </div><!--cierre div del inputt-->
+                </div> <!--cierre del div contenedor del input-->
+
+                <div class="form-group"> <!--Comienzo del div contenedor del input-->
+                    <label for="telCliente">id rol:</label>
+
+                    <div class="input-group my-colorpicker2 colorpicker-element"> <!--comienzo div del inputt-->
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-phone-alt"></i></span>
+                        <input type="text" class="form-control" placeholder="Ingrese su número teléfonico"
+                        name="telCliente" id="telCliente">
+
+                    </div><!--cierre div del inputt-->
+                </div> <!--cierre del div contenedor del input-->
+
+                <div class="form-group"> <!--Comienzo del div contenedor del input-->
+                    <label for="passCliente">id estado</label>
+
+                    <div class="input-group my-colorpicker2 colorpicker-element"> <!--comienzo div del inputt-->
+                        <span class="input-group-addon"><i class="fa fa-key"></i></span>
+                        <input type="password" class="form-control" placeholder="Ingrese su Contraseña"
+                        name="passCliente" id="passCliente">
+
+                    </div><!--cierre div del input-->
+                </div> <!--cierre del div contenedor del input-->
+
+            </div> <!--Cierre del Div contenedor-->
+            </form>
+
+            <div class="box-footer"> <!--Div que separa el formulario y contendrá los botones-->
+                <button type="button" class="btn btn-default">Cancelar</button>
+                <button type="button" class="btn btn-info pull-right" onclick="registrar()">Registrar</button>
+              </div> <!--Cierra Div que separa el formulario y contendrá los botones-->
           </div>
+
           </section>
           </div>
